6658f36db0e050c898b8ff8ca4ecd906f6d9a74a
PancaraBisma <pancarabisma@gmail.com>
2021-06-13 15:49:50 +0800

    Penambahan lokasi frame, judul frame, dan program tertutup ketika di close

--- src/GuiByText.java
+++ src/GuiByText.java
@@ -3,6 +3,9 @@ import javax.swing.*;
 public class GuiByText extends JFrame {
     GuiByText(){
         setSize(640, 480);
+        setLocationRelativeTo(null);
+        setTitle("Gui By Text:Pertemuan 4");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
